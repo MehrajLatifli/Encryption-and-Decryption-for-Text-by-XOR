ffb7d0553a87848953ba767eb8ea865b56f7a65f
Mehrac Lətifli <meracletifli@gmail.com>
2021-12-04 17:14:33 +0400

    Add project files.

---
+++ Encryption and Decryption for Text by XOR/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Encryption_and_Decryption_for_Text_by_XOR
+{
+    class Program
+    {
+        public static string XORCipher(string text, string key)
+        {
+            int dataLen = text.Length;
+            int keyLen = key.Length;
+            char[] output = new char[dataLen];
+
+            for (int i = 0; i < dataLen; ++i)
+            {
+                output[i] = (char)(text[i] ^ key[i % keyLen]);
+            }
+
+            return new string(output);
+        }
+
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            string cipherText = XORCipher("Hello World", "1234");
+            string plainText = XORCipher(cipherText, "1234");
+
+            Console.WriteLine(cipherText);
+            Console.WriteLine(plainText);
+
+            Console.ReadKey();
+        }
+    }
+}
